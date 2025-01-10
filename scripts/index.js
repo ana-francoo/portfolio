cf716cb62329a0797a2ccda458e435de7f544dbd
Ana Franco <alfranco@uwaterloo.ca>
2025-01-10 12:04:57 -0500

    home updated

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ import { Application } from '@splinetool/runtime';
 const canvas = document.getElementById('canvas3d');
 const spline = new Application(canvas);
 spline
-  .load('https://prod.spline.design/bHky9zqTjopWM3Ns/scene.splinecode')
+  .load('https://prod.spline.design/M4yHcdbcwkwBW-Sl/scene.splinecode') //
   .then(() => {
     spline.addEventListener('mouseDown', (e) => {
       if (e.target.id === 'f7b8d336-95f9-4f63-9fb6-ad1aff62cf06') {
@@ -12,3 +12,4 @@ spline
       }
     });
   });
+//https://prod.spline.design/bHky9zqTjopWM3Ns/scene.splinecode
