fc85986881a3e41ada9de78b7b69ba2933574daf
Ana Franco <alfranco@uwaterloo.ca>
2025-01-10 11:39:58 -0500

    first commit

---
+++ scripts/home.js
@@ -0,0 +1,7 @@
+import { Application } from "@splinetool/runtime";
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+
+// Load Scene B
+app.load('https://prod.spline.design/X67ZVLNp6xHJSj8t/scene.splinecode');
---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const spline = new Application(canvas);
+spline
+  .load('https://prod.spline.design/bHky9zqTjopWM3Ns/scene.splinecode')
+  .then(() => {
+    spline.addEventListener('mouseDown', (e) => {
+      if (e.target.id === 'f7b8d336-95f9-4f63-9fb6-ad1aff62cf06') {
+        window.location.href = '/home';
+        console.log("clicked");
+      }
+    });
+  });
