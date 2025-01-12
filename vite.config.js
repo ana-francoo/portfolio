1f6260a0d3eca6e56314c8a33e5a5db6db9c5dfb
Ana Franco <alfranco@uwaterloo.ca>
2025-01-11 21:10:57 -0500

    test

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: '/index.html'
+        main: '/index.html',
+        projects: '/projects/index.html'
       }
     }
   }
